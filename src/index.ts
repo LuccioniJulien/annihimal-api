d9ad90261aa36ba45fc6d9a44409762468a7a6cb
julien <julien.luccioni2014@gmail.com>
2019-06-21 14:45:33 +0200

    add tsnode

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import server from "./expess";
 
 const main = async () => {
   try {
-    const connection = await createConnection();
-    await connection.dropDatabase();
-    await connection.synchronize(true);
+    // const connection = await createConnection();
+    // await connection.dropDatabase();
+    // await connection.synchronize(true);
 
     const port: number = parseInt(process.env.PORT) || 4242;
     const isRunning: boolean = await server(port);

16de122cfc1987ab0ed06081f79eb1b0893e540c
Thomas PELFRENE <20171156@B15-13.local>
2019-06-21 15:11:32 +0200

    updating index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import { createConnection, Connection,  } from "typeorm";
 import server from "./expess";
+import { Animal } from "./models/animal";
 
 const main = async () => {
   try {
@@ -8,16 +9,38 @@ const main = async () => {
     await connection.dropDatabase();
     await connection.synchronize(true);
 
-    const port: number = parseInt(process.env.PORT) || 4242;
-    const isRunning: boolean = await server(port);
-    if (isRunning) {
-      console.log(`Server now running on port ${port}`);
-    } else {
-      throw "Something went wrong.....";
-    }
+    // const port: number = parseInt(process.env.PORT) || 4242;
+    // const isRunning: boolean = await server(port);
+    // if (isRunning) {
+    //   console.log(`Server now running on port ${port}`);
+    // } else {
+    //   throw "Something went wrong.....";
+    // }
+
+
+    // TEST - add an animal
+    await insertAnimal(connection)
+
   } catch (error) {
     console.log(error);
   }
 };
 
 main();
+
+
+/**
+ * ============================================================================
+ * helpers functions
+ * */
+
+const insertAnimal = (connection: Connection) => {
+  connection
+    .createQueryBuilder()
+    .insert()
+    .into(Animal)
+    .values([
+        { name: "Plop", color: "Yellow" }
+    ])
+    .execute();
+}
